ae738d50184ec3fd335512d78f0165d6d107c0d3
Khiem NV <nvkhiem2016@gmail.com>
2023-07-12 16:18:01 +0700

    first commit

--- components/BalanceDisplay.tsx
+++ components/BalanceDisplay.tsx
@@ -1,12 +1,22 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { FC, useEffect, useState } from "react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { getAccount, createAssociatedTokenAccountInstruction ,getAssociatedTokenAddressSync } from "@solana/spl-token";
 
 export const BalanceDisplay: FC = () => {
-  const [balance, setBalance] = useState(0);
-  const { connection } = useConnection();
-  const { publicKey } = useWallet();
+  const MOVE_MINT = new PublicKey(process.env.MOVE_TOKEN_MINT);
 
+  const [balance, setBalance] = useState(0);
+  const [tokenBalance, setTokenBalance] = useState(0);
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const getBalanceToken = async ()=>{
+    const account = getAssociatedTokenAddressSync(MOVE_MINT, publicKey);
+    // const ix = createAssociatedTokenAccountInstruction(publicKey, account, publicKey, MOVE_MINT);
+    // // sendTransaction(new Transaction().add(ix),connection)
+    const balance = await getAccount(connection, account);
+  }
   useEffect(() => {
     if (!connection || !publicKey) {
       return;
@@ -15,6 +25,7 @@ export const BalanceDisplay: FC = () => {
     connection.getAccountInfo(publicKey).then((info) => {
       setBalance(info.lamports);
     });
+    getBalanceToken();
   }, [connection, publicKey]);
 
   return (
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MOVE_TOKEN_MINT: 'To9y3LHENHDU4tU78ockkSZwVuxfvtgbSKPjqjXrNYt',
+  },
 }
 
 module.exports = nextConfig
